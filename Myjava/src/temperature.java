b6856ccb662c792f29541a8cd25e9b165e4378d1
apple7032 <apple70321228@gmail.com>
2019-11-16 17:38:16 +0800

    add MyJava temperature transform 判斷cf

--- Myjava/src/temperature.java
+++ Myjava/src/temperature.java
@@ -11,19 +11,31 @@
 public class temperature {
     public static void main(String [] args){
         
-        float c =23f, f;
         
-        System.out.print("請輸入攝氏溫度：");
-        java.util.Scanner sc = new java.util.Scanner(System.in);
-        double inputc = sc.nextDouble();
+  
+       double a, c, f;
+        System.out.println("請選擇");
+        System.out.println("1.C轉F");
+        System.out.println("2.F轉C");
+       java.util.Scanner sc = new java.util.Scanner(System.in);
+       double input = sc.nextDouble();
         
-        f =c*9/5+32;
-        System.out.println("華氏溫度為："+f);
-        
-        c=(f-32)*5/9;
-        System.out.println(c);
-        
-        
-
+        if (input == 1){
+            System.out.print("請輸入攝氏溫度：");
+            double inputc = sc.nextDouble();
+            f =inputc*9/5+32;
+            System.out.printf("華氏溫度為：%.2f",f);
+            
+        } 
+        if(input ==2){
+            System.out.print("請輸入華氏溫度：");
+            double inputf = sc.nextDouble();
+            c=(inputf-32)*5/9;
+            System.out.printf("攝氏溫度為：%.2f",c);
+        }
+        else{
+            System.out.println("請輸入數字1或2");
+        }
+       
     }
 }
